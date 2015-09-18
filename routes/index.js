4b6231808efffbf69e7f7aa1040dfe52398c5c92
astigmatism <Astigmatism@Samus.local>
2015-09-18 14:36:47 -0700

    fixed conflicts

--- routes/index.js
+++ routes/index.js
@@ -71,19 +71,11 @@ router.get('/load/:system/:title/:file', function(req, res, next) {
             return res.json(err);
         }
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-        UtilitiesService.setPlayHistory(req, system, title, file, function() {
-            res.send(data);
-=======
->>>>>>> Stashed changes
         UtilitiesService.setPlayHistory(req, system, title, file, true, function(err, ph) {
             if (err) {
                 return res.json(err);
             }
-            res.send(new Buffer(data, 'binary'));
->>>>>>> origin/master
+            res.send(data);
         });
     });
 });
