4306b083fa4df6cb4ee4c7a6f99ebf6ab67c9a93
astigmatism <eric@crazyerics.com>
2016-01-29 10:20:21 -0800

    more stuf

--- public/javascripts/crazyerics.js
+++ public/javascripts/crazyerics.js
@@ -737,7 +737,7 @@ Crazyerics.prototype._setupKeypressInterceptor = function(system, title, file) {
                             self._Module.requestFullScreen(true, true);
                         break;
                         case 67: //c
-                            self.getShader('bicubic-normal');
+                            self.getShader('gb-shader');
                         break;
                     }
                 break;
