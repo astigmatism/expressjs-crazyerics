c9737ed1f898d75072a0c540371d65c95fc707bf
Eric Glomstad <eric.glomstad@igt.com>
2017-05-18 16:29:11 -0700

    progress bar start

--- public/javascripts/ces.main.js
+++ public/javascripts/ces.main.js
@@ -186,6 +186,36 @@ var cesMain = (function() {
             }
         });
 
+        //progress bar
+        var bar = new ProgressBar.Line(loadingprogressbar, {
+            strokeWidth: 4,
+            easing: 'easeInOut',
+            duration: 1400,
+            color: '#FFEA82',
+            trailColor: '#eee',
+            trailWidth: 1,
+            svgStyle: {width: '100%', height: '100%'},
+            text: {
+                style: {
+                // Text color.
+                // Default: same as stroke color (options.color)
+                color: '#999',
+                position: 'absolute',
+                right: '0',
+                top: '30px',
+                padding: 0,
+                margin: 0,
+                transform: null
+                },
+                autoStyleContainer: false
+            },
+            from: {color: '#FFEA82'},
+            to: {color: '#ED6A5A'},
+            step: (state, bar) => {
+                bar.setText(Math.round(bar.value() * 100) + ' %');
+            }
+        });
+
         //clicking on paused game overlay
         $('#emulatorwrapperoverlay')
             .on('click', function() {
@@ -452,6 +482,8 @@ var cesMain = (function() {
                                             //load state? bails if null.. if valid, will show a new save loading dialog
                                             //and will load state. callback occurs after state has loaded
                                             LoadEmulatorState(system, stateToLoad, function() {
+
+                                                return;
 
                                                 //close all dialogs (save loading or game loading), game begins!
                                                 _Dialogs.CloseDialog(false, function() {
