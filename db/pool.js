c8dfafaceb828d43f80141741fac883e45edfedd
astigmatism <eric@crazyerics.com>
2017-09-27 11:16:02 -0700

    password in external file

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,24 @@
 //The pool should be a long-lived object in your application. Generally you'll want to instantiate one pool when your app starts up and use the same instance of the pool throughout the lifetime of your application. If you are frequently creating a new pool within your code you likely don't have your pool initialization code in the correct place
-var config = require('config');
+'use strict';
+const fs = require('fs');
+const config = require('config');
 const { Pool } = require('pg');
 
 // correct usage: create the pool and let it live
 // 'globally' here, controlling access to it through exported methods
-const pool = new Pool(config.get('db.postgre'));
+
+var pool = null;
+var queue = [];
+
+//password is saved in an extermal file NOT checked in to source control
+fs.readFile(__dirname + '/../config/dbpass.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.log('Error obtaining db password');
+    }
+    var connection = config.get('db.postgre');
+    connection.password = data;
+    pool = new Pool(connection);
+});
 
 // this is the right way to export the query method
 module.exports.query = (text, values, callback) => {
@@ -13,6 +27,9 @@ module.exports.query = (text, values, callback) => {
     //console.log('query:', text, values);
     console.log('query: ' + text);
     
+    //if the pool is not up yet, bail and ignore
+    if (!pool) return;
+
     return pool.query(text, values, callback);
 }
 
