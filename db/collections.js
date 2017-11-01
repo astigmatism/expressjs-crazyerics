e88b2a03cc7d435748834f99ad72c0b35402927c
astigmatism <eric@crazyerics.com>
2017-11-01 16:29:15 -0700

    fixed timestamps for collections, refactored collecgtions

--- db/collections.js
+++ db/collections.js
@@ -37,15 +37,15 @@ module.exports = new (function() {
     };
 
     this.PlayCollectionTitle = function(userId, collectionId, gk, titleId, fileId, callback) {
-        
-        pool.query('UPDATE collections_titles SET active_file=$3, last_played=$4, play_count=play_count+1, game_key=$5 WHERE collection_id=$1 AND title_id=$2 RETURNING *', [collectionId, titleId, fileId, new Date, gk], (err, result) => {
+
+        pool.query('UPDATE collections_titles SET active_file=$3, last_played=now(), play_count=play_count+1, game_key=$4 WHERE collection_id=$1 AND title_id=$2 RETURNING *', [collectionId, titleId, fileId, gk], (err, result) => {
             if (err) {
                 return callback(err);
             }
             //if update failed to return rows, add this
             if (result.rows.length === 0) {
                 
-                pool.query('INSERT INTO collections_titles (collection_id, title_id, active_file, game_key) VALUES ($1, $2, $3, $4) RETURNING *', [collectionId, titleId, fileId, gk], (err, result) => {
+                pool.query('INSERT INTO collections_titles (collection_id, title_id, active_file, last_played, game_key) VALUES ($1, $2, $3, now(), $4) RETURNING *', [collectionId, titleId, fileId, gk], (err, result) => {
                     if (err) {
                         return callback(err);
                     }
--- public/javascripts/ces.collections.js
+++ public/javascripts/ces.collections.js
@@ -2,10 +2,15 @@ var cesCollections = (function(config, _Compression, _Sync, _Tooltips, _PlayGame
 		
     //private members
     var _self = this;
-    var _collections = [];
-    var _active = {};
+    var _grid = null; //see constructor for assignment
+    var _activeCollectionData = {};
+    var _activeCollectionTitles = [];
     var _BOXSIZE = 120;
     var _currentLoadingGame = null;
+
+    //original incoming data from sync
+    var _collections = [];
+    var _active = {};
 
 	//public members
 
@@ -31,9 +36,96 @@ var cesCollections = (function(config, _Compression, _Sync, _Tooltips, _PlayGame
     this.RemoveCurrentGameLoading = function() {
         _currentLoadingGame = null;
     };
+    
+    // var Remove = function(gk, gamelink, griditem) {
+
+    //     //before removing, is this the current game being loaded? 
+    //     //we cannot allow it to be deleted (like if there are selecting a save)
+    //     if (_currentLoadingGame && _currentLoadingGame.hasOwnProperty('gk') && gk == _currentLoadingGame.gk) {
+    //         return;
+    //     }
+
+    //     //maybe set a loading spinner on image here?
+    //     gamelink.DisableAllEvents(); //disabled buttons on gamelink to prevent loading game or removing again
+
+    //     //immediately remove from grid (i used to wait for response but why right?)
+    //     _grid.isotope('remove', griditem).isotope('layout');
+
+    //     var url = '/collections/game?gk=' + encodeURIComponent(gk);
+
+    //     _Sync.Delete(url, function(data) {
+    //         //sync will take care of updating the collection
+    //     });
+    // };
+
+    //examines the local cache about the active collection and populates the grid as needed
+    this.Populate = function() {
+
+        var gridTitles = _grid.isotope('getItemElements');
+
+        //go through all titles in cache
+        for (var i = 0, len = _activeCollectionTitles.length; i < len; ++i) {
+
+            var activeTitle = _activeCollectionTitles[i];
+
+            //does this title already exist in the grid?
+            var foundInGrid = false;
+            for (var j = 0, jlen = gridTitles.length; j < jlen; ++j) {
+
+                var $gridTitle = $(gridTitles[j]);
+                var gridGk = $gridTitle.data('gk');      //take the gk from the element for comparison. will be unique
+
+                if (gridGk === activeTitle.gameKey.gk) {
+                    foundInGrid = true;
+
+                    //found this title in the grid, update its attributes to keep it up to date
+                    $gridTitle.attr('data-lastPlayed', activeTitle.lastPlayed); //store as epoch time for sorting
+                    
+                }
+            }
+
+            if (!foundInGrid) {
+                Add(activeTitle);
+            }
+
+            //ok, by this time the title should be on the grid and updated
+            var $tooltipContent = GenerateTooltipContent(activeTitle, i);
+            activeTitle.gameLink.UpdateToolTipContent($tooltipContent);
+        }
+
+        //finally, sort everything
+        _self.SortBy('lastPlayed', false);
+
+        OnImagesLoaded();
+    };
+
+    var Add = function(activeTitle) {
+        
+        //create the grid item
+        var $griditem = $('<div class="grid-item" />');
+
+        //place sorting data on grid item
+        $griditem.attr('data-gk', activeTitle.gameKey.gk);
+        $griditem.attr('data-lastPlayed', activeTitle.lastPlayed); //store as epoch time for sorting
+
+        $griditem.append(activeTitle.gameLink.GetDOM()); //add all visual content from gamelink to grid
+        
+        _grid.isotope('insert', $griditem[0]);
+    };
+
+    var GenerateTooltipContent = function(activeTitle, index) {
+
+        //create the tooltip content
+        var $tooltipContent = $('<div class="collection-tooltip" id="collection' + index + '"></div>');
+        $tooltipContent.append('<div>' + activeTitle.gameKey.title + '</div>');
+        $tooltipContent.append('<div>Last Played: ' + $.format.date(activeTitle.lastPlayed, 'MMM D h:mm:ss a') + '</div>'); //using the jquery dateFormat plugin
+        $tooltipContent.append('<div>Play Count: ' + activeTitle.playCount + '</div>');
+
+        return $tooltipContent;
+    };
 
     var OnImagesLoaded = function() {
-
+        
         _grid.find('img').imagesLoaded().progress(function(imgLoad, image) {
             
             $(image.img).parent().removeClass('close'); //remove close on parent to reveal image
@@ -42,153 +134,13 @@ var cesCollections = (function(config, _Compression, _Sync, _Tooltips, _PlayGame
         });
     };
 
-	/**
-     * Assign actions to a gamelink and then append it to the grid but does NOT perform grid layout
-     * @param {[type]}   key      [description]
-     * @param {[type]}   system   [description]
-     * @param {[type]}   title    [description]
-     * @param {[type]}   file     [description]
-     * @param {[type]}   played   [description]
-     * @param {[type]}   slots    [description]
-     * @param {Function} callback [description]
-     */
-	var AddToGrid = function(game, index) {
-
-        var gameKey = _Compression.Decompress.gamekey(game.gk);
-
-        //create the grid item
-        var $griditem = $('<div class="grid-item" />');
-
-        //if the box image fails to load, resync this grid to make room for the error images
-        var onBoxImageLoadError = function(el) {
-            _grid.isotope('layout');
-        };
-
-		var gamelink = new cesGameLink(config, gameKey, _BOXSIZE, true, _PlayGameHandler, onBoxImageLoadError);
-
-        //create tooltip content
-        var $tooltipContent = Tooltip(game, index);
-        gamelink.UpdateToolTipContent($tooltipContent);
-
-        //set the on remove function
-        gamelink.OnRemoveClick(function() {
-            //Remove(game.gk, gamelink, $griditem);
-        });
-
-        //place sorting data on grid item
-        $griditem.attr('data-gk', game.gk);
-        $griditem.attr('data-lastPlayed', new Date(game.lastPlayed).getTime()); //store as epoch time for sorting
-
-        $griditem.append(gamelink.GetDOM()); //add gamelink
-        
-        _grid.isotope('insert', $griditem[0]);
-
-        game.gameLink = gamelink;
-        game.griditem = $griditem;
-    };
-    
-    var Tooltip = function(game, index) {
-
-        var gameKey = _Compression.Decompress.gamekey(game.gk);
-
-        //create the tooltip content
-        var $tooltipContent = $('<div class="collection-tooltip" id="collection' + index + '"></div>');
-        $tooltipContent.append('<div>' + gameKey.title + '</div>');
-        $tooltipContent.append('<div>Last Played: ' + $.format.date(game.lastPlayed, 'MMM D h:mm:ss a') + '</div>'); //using the jquery dateFormat plugin
-        $tooltipContent.append('<div>Play Count: ' + game.playCount + '</div>');
-
-        return $tooltipContent;
-    };
-
-    
-    /**
-     * @param  {String} gameKey
-     * @param  {Object} gamelink
-     * @param  {Object} griditem
-     */
-    var Remove = function(gk, gamelink, griditem) {
-
-        //before removing, is this the current game being loaded? 
-        //we cannot allow it to be deleted (like if there are selecting a save)
-        if (_currentLoadingGame && _currentLoadingGame.hasOwnProperty('gk') && gk == _currentLoadingGame.gk) {
-            return;
-        }
-
-        //maybe set a loading spinner on image here?
-        gamelink.DisableAllEvents(); //disabled buttons on gamelink to prevent loading game or removing again
-
-        //immediately remove from grid (i used to wait for response but why right?)
-        _grid.isotope('remove', griditem).isotope('layout');
-
-        var url = '/collections/game?gk=' + encodeURIComponent(gk);
-
-        _Sync.Delete(url, function(data) {
-            //sync will take care of updating the collection
-        });
-    };
-
-    //populate clears the grid from scratch
-    this.Populate = function() {
-
-        _grid.isotope('remove', _grid.children()); //clear grid
-
-        for (var i = 0, len = _active.titles.length; i < len; ++i) {
-            var game = _active.titles[i];
-            
-            //add to grid returns handle to griditem and gamelink
-            AddToGrid(game, i);
-        }
-
-        _self.SortBy('lastPlayed', false);
-
-        OnImagesLoaded();
-    };
-
-    //updates the grid with new titles (or deleted ones)
-    this.Refresh = function() {
-
-        var items = _grid.isotope('getItemElements');
-
-        //step through all updated active titles
-        for (var i = 0, len = _active.titles.length; i < len; ++i) {
-            var game = _active.titles[i];
-
-            var found = false;
-            for (var j = 0, jlen = items.length; j < jlen; ++j) {
-
-                var $griditem = $(items[j]);
-                var gk = $griditem.data('gk');
-
-                if (gk === game.gk) {
-                    found = true;
-                    
-                    //update details
-                    $griditem.attr('data-lastPlayed', new Date(game.lastPlayed).getTime()); //store date in epoch time for sorting
-
-                    if (game.gameLink) {
-                        var $tooltipContent = Tooltip(game, index);
-                        game.gamelink.UpdateToolTipContent($tooltipContent);   
-                    }
-                    
-                    break;
-                }
-            }
-            if (!found) {
-                AddToGrid(game, i);
-            }
-        }
-
-        _self.SortBy('lastPlayed', false);
-        
-        OnImagesLoaded();
-    };
-
     //in order to sync data between server and client, this structure must exist
     this.Sync = new (function() {
 
         var __self = this;
         this.ready = false;
 
+        //a package is the entire payload of data shared between client and server
         var package = (function(active, collections) {
             this.active = active;
             this.collections = collections;
@@ -196,28 +148,72 @@ var cesCollections = (function(config, _Compression, _Sync, _Tooltips, _PlayGame
 
         this.Incoming = function(package) {
 
-            var isNewCollection = true; 
-            if (_active.data) {
-                isNewCollection = (_active.data.name != package.active.data.name);
+            var isNewCollection = true;
+            
+            //reset local cache on incoming data
+            _activeCollectionTitles = [];
+            _activeCollectionData = {};
+
+            //locally cache data
+            if (package.active.hasOwnProperty('titles')) {
+                var payload = package.active.titles;
+
+                //let's set up our local cache with massaged data appropriate for consumption
+                for (var i = 0, len = payload.length; i < len; ++i) {
+
+                    //decompress gk
+                    var gameKey = _Compression.Decompress.gamekey(payload[i].gk);
+                    
+                    //get timezone correct last played date
+                    var timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000; //convert from minutes to mili
+                    var utcDate = new Date(payload[i].lastPlayed);
+                    var utcTime = utcDate.getTime();
+                    var lastPlayed = utcTime - timezoneOffset;
+
+                    //if the box image fails to load, resync this grid to make room for the error images
+                    var onBoxImageLoadError = function(el) {
+                        _grid.isotope('layout');
+                    };
+
+                    //generate gamelink
+                    var gameLink = new cesGameLink(config, gameKey, _BOXSIZE, true, _PlayGameHandler, onBoxImageLoadError);
+
+                    //set the on remove function
+                    gameLink.OnRemoveClick(function() {
+                        //Remove(game.gk, gamelink, $griditem);
+                    });
+
+                    //push to our local cache
+                    _activeCollectionTitles.push({
+                        gameKey: gameKey,
+                        lastPlayed: lastPlayed,
+                        lastPlayedServerDate: utcDate,
+                        playCount: payload[i].playCount,
+                        gameLink: gameLink
+                    });
+                }
+            }
+            if (package.active.hasOwnProperty('data')) {
+
+                //determine if this collection is not the collection currently on display
+                if (_activeCollectionData.hasOwnProperty('name') && package.active.data.hasOwnProperty('name')) {
+                    isNewCollection = (_activeCollectionData.name != package.active.data.name);
+                }
+                _activeCollectionData = package.active.data;
             }
 
+            //save incoming data for later use in outgoing in needed
             _active = package.active;
             _collections = package.collections;
 
+            //if this is entire package contains data for a new collection not currently being shown, clear the grid
             if (isNewCollection) {
-                _self.Populate();
-            }
-            else {
-                _self.Refresh();
+                _grid.isotope('remove', _grid.children()); //clear grid first
+                
             }
 
-            //title
-            // if (_active.titles.length > 0) {
-            //     $title.text(_active.data.name);
-            // }
-            // else {
-            //     $title.empty();
-            // }
+            //populate updates grid
+            _self.Populate();
         };
 
         //not used (yet). delete forces update on server
@@ -235,6 +231,7 @@ var cesCollections = (function(config, _Compression, _Sync, _Tooltips, _PlayGame
      */
     var Constructor = (function() {
         
+        //first, build the grid
         _grid = $wrapper.isotope({
             layoutMode: 'masonry',
             itemSelector: '.grid-item',
@@ -246,6 +243,7 @@ var cesCollections = (function(config, _Compression, _Sync, _Tooltips, _PlayGame
             }
         });
 
+        //parse the incoming sync data from server
         _self.Sync.Incoming(_initialSyncPackage);
 
     })();
